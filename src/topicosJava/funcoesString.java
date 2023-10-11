a3ab2cdbaf42bedf08a23d216ce8b5a41ee318fe
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-11 18:49:39 -0300

    add primeiro exemplo de substring

--- src/topicosJava/funcoesString.java
+++ src/topicosJava/funcoesString.java
@@ -1,4 +1,3 @@
-
 package topicosJava;
 
 /**
@@ -8,19 +7,21 @@ package topicosJava;
 public class funcoesString {
 
     public static void main(String[] args) {
-      
+
         String original = "abcde FGHIJ ABB abc DEFG ";
-     
-        
+
         String s01 = original.toLowerCase();// minusculo
         String s02 = original.toUpperCase();//maiusculo
         String s03 = original.trim(); //remove os espacos mantem a mesma formatacao
-        
-        System.out.println("original: -"  + original + "-");  
-        
-        System.out.println("toLowerCase: -" + s01 + "-");     
+        String s04 = original.substring(2); // mostrar apartir do c caracter  da segunda posicao em diante nesse caso vai ser o "c"
+
+        System.out.println("original: -" + original + "-");
+
+        System.out.println("toLowerCase: -" + s01 + "-");
         System.out.println("toUpperCase: -" + s02 + "-");
         System.out.println("trim: -" + s03 + "-");
+        System.out.println("substring(2): -" + s04 + "-");
+
     }
-    
+
 }
