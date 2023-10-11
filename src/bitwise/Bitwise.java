8b130689ad4dcf04a3da839524c8e305e09f5458
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-11 18:07:04 -0300

    add exercicio bitwise

---
+++ src/bitwise/Bitwise.java
@@ -0,0 +1,28 @@
+package bitwise;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Bitwise {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int mask = 0b100000; // ou 32
+
+        int n = sc.nextInt();
+
+        if ((n & mask) != 0) {
+            System.out.println("6th bit is true!");
+        } else {
+            System.out.println("6th bit is false");
+        }
+        sc.close();
+
+    }
+
+}
