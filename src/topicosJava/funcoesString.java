def94057f8b0ade70cde12b8475b7f0fe5f7de74
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-11 18:41:33 -0300

    add funcao trim

--- src/topicosJava/funcoesString.java
+++ src/topicosJava/funcoesString.java
@@ -14,11 +14,13 @@ public class funcoesString {
         
         String s01 = original.toLowerCase();// minusculo
         String s02 = original.toUpperCase();//maiusculo
+        String s03 = original.trim(); //remove os espacos mantem a mesma formatacao
         
         System.out.println("original: -"  + original + "-");  
         
         System.out.println("toLowerCase: -" + s01 + "-");     
         System.out.println("toUpperCase: -" + s02 + "-");
+        System.out.println("trim: -" + s03 + "-");
     }
     
 }
