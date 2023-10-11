6b75a921736a6b75961945406034f2bd478583c4
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-11 18:39:03 -0300

    add funcao UpperCase

--- src/topicosJava/funcoesString.java
+++ src/topicosJava/funcoesString.java
@@ -10,11 +10,15 @@ public class funcoesString {
     public static void main(String[] args) {
       
         String original = "abcde FGHIJ ABB abc DEFG ";
+     
         
-        String s01 = original.toLowerCase();
+        String s01 = original.toLowerCase();// minusculo
+        String s02 = original.toUpperCase();//maiusculo
         
         System.out.println("original: -"  + original + "-");  
+        
         System.out.println("toLowerCase: -" + s01 + "-");     
+        System.out.println("toUpperCase: -" + s02 + "-");
     }
     
 }
