8af0bf3411a0db334a52eac38c26d4e6c0642544
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-12 18:48:03 -0300

    add method toString

--- src/orientacaoObjetos/Product.java
+++ src/orientacaoObjetos/Product.java
@@ -1,4 +1,3 @@
-
 package orientacaoObjetos;
 
 /**
@@ -6,22 +5,28 @@ package orientacaoObjetos;
  * @author grazielle
  */
 public class Product {
-    
+
     public String name;
     public double price;
     public int quantity;
-    
-    public double totalValueInStock(){
+
+    public double totalValueInStock() {
         return price * quantity;
-        
+
     }
-    public void addProducts(int quantity ){
-       this.quantity += quantity;
-        
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+
     }
     
-    public void removeProducts(int quantity){
-        this.quantity -= quantity;
+    public String toString(){
+        return name;
         
     }
 }
