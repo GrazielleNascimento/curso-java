61c7e800e1d6249f11b4c1cc6263b124ef9a36a0
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-12 14:35:11 -0300

    add class Triangle com orientacao a objetos

---
+++ src/entities/Triangle.java
@@ -0,0 +1,17 @@
+
+package entities;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Triangle {
+    
+    public double a;
+    public double b;
+    public double c;
+    
+    
+    
+    
+}
