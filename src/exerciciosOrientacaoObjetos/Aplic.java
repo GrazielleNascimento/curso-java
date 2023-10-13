06f66e92bee9e804448370b6a62b08c87a4870ea
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-12 21:40:51 -0300

    add main class Aplic

---
+++ src/exerciciosOrientacaoObjetos/Aplic.java
@@ -0,0 +1,31 @@
+package exerciciosOrientacaoObjetos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Aplic {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Width: ");
+        rectangle.width = sc.nextDouble();
+
+        System.out.println("Height: ");
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("Area: " + rectangle.area());
+
+        System.out.println("Perimeter: " + rectangle.perimeter());
+        
+        System.out.println("Diagonal: " + rectangle.diagonal());
+    }
+
+}
