1472aa2317129517e8fff207894640e321650821
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-13 23:21:06 -0300

    add main class Aplic

---
+++ src/exerciciosOrientacaoObjetos/exe4/Aplic.java
@@ -0,0 +1,27 @@
+
+package exerciciosOrientacaoObjetos.exe4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Aplic {
+
+    public static void main(String[] args) {
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+               
+        System.out.print("What is the dollar price?: ");  
+        double dollarPrice = sc.nextDouble();
+         
+        System.out.print("How many dollars will be bought?: ");
+        double qtdDollar = sc.nextDouble();
+         
+        System.out.printf("Amount to be paid in reais: %.2f%n" , CurrencyConverter.dolarToReal(dollarPrice, qtdDollar));
+        
+    }
+    
+}
