25aeeaf01cd2b1e5e6dd0526b55941e33c204af6
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-14 18:21:49 -0300

    add Product class with constructor

---
+++ src/construtor/Aplic.java
@@ -0,0 +1,48 @@
+package construtor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Aplic {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Product data: ");
+        System.out.print("Name: ");
+        String name = sc.next();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Removed data: " + product);
+
+        sc.close();
+    }
+
+}
