b68ee74db0dd26463fcc4966ab7c76e3d32ae850
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-15 18:48:45 -0300

    add Product class with encapsulation

---
+++ src/encapsulamento/Product.java
@@ -0,0 +1,78 @@
+
+package encapsulamento;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+    
+    
+    public Product(String name, double price) { // sobrecarga
+        this.name = name;
+        this.price = price;
+       
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }   
+    
+
+    public double totalValueStock() {
+        return price * quantity;
+
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+                
+
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+        
+    }
+    public String toString(){
+        return name 
+                + ", $ " 
+                + String.format("%.2f",  price )  
+                + ", "
+                +  quantity
+                + " units,   Total: $  "
+                + String.format("%.2f", totalValueStock());
+        
+    }
+    
+
+}
