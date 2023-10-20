903b88732be3747dbecf34745208489b7b8dc925
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-20 09:51:55 -0300

    add main class Vetor

---
+++ src/arrays/Vetor.java
@@ -0,0 +1,44 @@
+
+package arrays;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author grazielle
+ */
+public class Vetor {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.println("Informe o numero de alturas: ");
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+        
+        for(int i = 0; i < n; i++){
+            
+            System.out.println("Informe as alturas: ");          
+            vect[i] = sc.nextDouble();
+                                                                                                                       
+        
+        }
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
