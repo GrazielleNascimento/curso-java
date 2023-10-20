b54d0abbed9ed6a387c74527a3de4eb78a321a3c
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-20 10:10:20 -0300

    added average height

--- src/arrays/Vetor.java
+++ src/arrays/Vetor.java
@@ -1,4 +1,3 @@
-
 package arrays;
 
 import java.util.Locale;
@@ -11,34 +10,30 @@ import java.util.Scanner;
 public class Vetor {
 
     public static void main(String[] args) {
-        
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
-        
+
         System.out.println("Informe o numero de alturas: ");
         int n = sc.nextInt();
         double[] vect = new double[n];
-        
-        for(int i = 0; i < n; i++){
-            
-            System.out.println("Informe as alturas: ");          
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Informe as alturas: ");
             vect[i] = sc.nextDouble();
-                                                                                                                       
-        
         }
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT: %.2f%n" , avg);
+
+        sc.close();
+
     }
-    
+
 }
