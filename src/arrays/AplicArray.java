57c4b96804973734ec550f5d0d3b42b99c7b6919
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-25 22:24:36 -0300

    for com ArrayList

--- src/arrays/AplicArray.java
+++ src/arrays/AplicArray.java
@@ -1,4 +1,3 @@
-
 package arrays;
 
 import java.util.ArrayList;
@@ -10,24 +9,26 @@ import java.util.ArrayList;
 public class AplicArray {
 
     public static void main(String[] args) {
-    
 
         ArrayList<String> carros = new ArrayList<>();
-           
+
         carros.add("HRV");
         carros.add("Polo");
         carros.add("Cruze");
         carros.add("Argo");
         carros.add("Fusion");// a ultima insercao tem maior preferencia agora o Argo nao esta mais na 1 posicao mas na segunda do arrayList
-        
-        System.out.println(carros.get(1));
+
+        for (int i = 0; i < carros.size(); i++) {
+
+            System.out.println(carros.get(i));
+
+        }
+
         // imprimir apenas um elemento especifico 
         // sem mostrar o intervalo completo
-        
-    /*    for(String e:carros){
+        /*    for(String e:carros){
             System.out.println(e);
         }*/
-             
     }
-    
+
 }
