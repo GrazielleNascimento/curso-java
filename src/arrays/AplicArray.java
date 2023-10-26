f5444d7079c38105888a7115457cf1012e9c9959
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-25 22:08:13 -0300

    add exemplo ArrayList integer com for-each

---
+++ src/arrays/AplicArray.java
@@ -0,0 +1,27 @@
+
+package arrays;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author grazi
+ */
+public class AplicArray {
+
+    public static void main(String[] args) {
+    
+        ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<String> carros = new ArrayList<>();
+        
+        numeros.add(11);
+        numeros.add(22);
+        numeros.add(33);     
+         
+        for(int e:numeros){
+            System.out.println(e);
+        }
+             
+    }
+    
+}
