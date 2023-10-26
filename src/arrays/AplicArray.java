3528c1d1f2b0f1a8e63ed1ee2df6b094fa9c6ea5
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-25 22:15:08 -0300

    add alteracao de posicionamento de um arrayList

--- src/arrays/AplicArray.java
+++ src/arrays/AplicArray.java
@@ -11,21 +11,13 @@ public class AplicArray {
 
     public static void main(String[] args) {
     
-        ArrayList<Integer> numeros = new ArrayList<>();
+
         ArrayList<String> carros = new ArrayList<>();
-        
-        numeros.add(11);
-        numeros.add(22);
-        numeros.add(33); 
-        
+           
         carros.add("HRV");
         carros.add("Polo");
         carros.add("Cruze");
-         
-        //for each
-        for(int e:numeros){
-            System.out.println(e);
-        }
+        carros.add(1,"Argo");
         
         for(String e:carros){
             System.out.println(e);
