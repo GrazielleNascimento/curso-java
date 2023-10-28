4912d129e0e7da48e2b5d02319c5c0a5ac42da80
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-27 22:12:42 -0300

    add mais conceitos

--- src/arrays/AplicClientes.java
+++ src/arrays/AplicClientes.java
@@ -1,4 +1,3 @@
-
 package arrays;
 
 import java.util.ArrayList;
@@ -9,17 +8,22 @@ import java.util.ArrayList;
  */
 public class AplicClientes {
 
-
     public static void main(String[] args) {
-        ArrayList<String> clientes = new ArrayList <String>();
-        
+        ArrayList<String> clientes = new ArrayList<String>();
+
         clientes.add("Rebeca");
         clientes.add("Perla");
-        System.out.println(clientes.get());
-      //  clientes.remove(1); remover apartir do indice
-      //clientes.clear();// remove tudo
-      //System.out.println(clientes.toString());// mostra todos itens do array
         
+        // Metodos e conceitos do ArrayList
+        
+       // System.out.println(clientes.indexOf("Rebeca")); -- retorna a posicao do item no array
+       // System.out.println(clientes.size()); --- mostra a qtd de itens no array
+       // System.out.println(clientes.contains("Aline")); --- testa se determinado item esta no array
+        //System.out.println(clientes.isEmpty()); ---- pergunta se o array esta vazio
+        //System.out.println(clientes.get(0)); --- mostrar pelo indice
+        //clientes.remove(1); ----remover apartir do indice
+        //clientes.clear(); ------ remove tudo
+        //System.out.println(clientes.toString()); ---- mostra todos itens do array
     }
-    
+
 }
