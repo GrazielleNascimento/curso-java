9e5640f4458d10e1364a175ce04d037becb25e5f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-27 22:16:58 -0300

    add primeiro exemplo do uso de Array

---
+++ src/arrays/Array.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author grazi
+ */
+public class Array {
+
+ 
+    public static void main(String[] args) {
+        
+        int n1 = 5;
+        
+        int[] numeros;
+        numeros = new int[3];
+        
+        numeros[0] = 2;
+        
+        System.out.println(n1);// mostra apenas a variavel
+        
+        System.out.println(numeros[1]); // mostra o valor no Array da posicao informada
+        
+    }
+    
+}
