d24a9b4ee6c632b8b02d3cc4a7fa1528c7b9c4af
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-27 21:51:48 -0300

    add AplicClientes ArrayList metodos

---
+++ src/arrays/AplicClientes.java
@@ -0,0 +1,25 @@
+
+package arrays;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author grazi
+ */
+public class AplicClientes {
+
+
+    public static void main(String[] args) {
+        ArrayList<String> clientes = new ArrayList <String>();
+        
+        clientes.add("Rebeca");
+        clientes.add("Perla");
+        System.out.println(clientes.get());
+      //  clientes.remove(1); remover apartir do indice
+      //clientes.clear();// remove tudo
+      //System.out.println(clientes.toString());// mostra todos itens do array
+        
+    }
+    
+}
