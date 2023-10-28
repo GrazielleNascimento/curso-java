5610261b4efc997dee86becc93c32e47648b4c63
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-10-28 15:51:26 -0300

    add classe CarinhoDeCompras  com ArrayList

---
+++ src/carrinhodecompra/CarrinhoDeCompras.java
@@ -0,0 +1,43 @@
+
+package carrinhodecompra;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author grazi
+ */
+public class CarrinhoDeCompras {
+    
+    ArrayList<Produto> carrinho = new ArrayList<>();
+    
+    public void addProdutos(Produto produto){
+        carrinho.add(produto);
+    }
+    public void removeProduto(Produto produto){
+        carrinho.remove(produto); 
+    }
+    public double totalPreco(){
+        double somaPreco = 0.0;
+        
+        for(Produto produto : carrinho){
+            
+            somaPreco += produto.getPreco();
+        } 
+        
+        return somaPreco;
+    }
+    
+    @Override
+    public String toString(){
+       StringBuilder sb = new StringBuilder();
+       
+       for(Produto produto : carrinho) {
+           sb.append("\n" + produto + "\n");
+           
+       }
+       return sb.toString();
+       
+    } 
+    
+}
