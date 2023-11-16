bec7a8a0bb75a8d28efb1b7bf51c68c2e4e57412
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-15 23:27:40 -0300

    add metodo contains

--- src/java_exercicios/ExSet.java
+++ src/java_exercicios/ExSet.java
@@ -15,10 +15,15 @@ public class ExSet {
 		carros.add("HRV");
 		carros.add("Polo");
 		carros.add("HRV");
+
+		System.out.println(carros.add("Polo") ? "SIM" : "Não"); //false, pois ja esta adicionado
 		
+		System.out.println(carros.add("Fit") ? "SIM" : "Não");// true, pois não existia na lista
 		
-		System.out.println(carros);
-		
+		System.out.println(carros);// exibe a lista
+
+		System.out.println(carros.contains("Fit") ? "SIM" : "Não"); //contains verifica se esta na lista
+
 		
 
 	}
