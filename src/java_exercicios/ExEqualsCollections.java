836d1656a65595196e5727a5642c687599217be2
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-15 22:28:38 -0300

    add ExEquals

---
+++ src/java_exercicios/ExEqualsCollections.java
@@ -0,0 +1,13 @@
+package java_exercicios;
+
+public class ExEqualsCollections {
+
+	public static void main(String[] args) {
+		String nome = "William Suane";
+		String nome2 = "William Suane";
+		
+		System.out.println(nome.equals(nome2));// mostra que é igual
+
+	}
+
+}
