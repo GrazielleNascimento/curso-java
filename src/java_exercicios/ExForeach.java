384afe7fb9a741338dabd62e71e2bfbe7b87c6b4
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-15 22:28:52 -0300

    add exForeach

---
+++ src/java_exercicios/ExForeach.java
@@ -0,0 +1,17 @@
+package java_exercicios;
+
+public class ExForeach {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = new int[3];
+		int[] numeros2 = {1,2,3,4,5};
+		int[] numeros3 = new int[] {5,4,3,2,1};
+		
+		for (int num : numeros2) {
+			System.out.println(num);
+		}
+
+	}
+
+}
